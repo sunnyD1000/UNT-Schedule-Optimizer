fdfb5867a900ed9061469ee938e934c067a14347
Sangharsha Dhakal <sangharshadhakal@my.unt.edu>
2026-02-28 17:17:22 -0600

    Hide secret variable

--- UNTSO/settings.py
+++ UNTSO/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+DJANGO_SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6+y$z&(f64^fn9i0*6p&4zc&xvi=mnd%$1ho0!e%n^)dvgv_+4'
+SECRET_KEY = DJANGO_SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
